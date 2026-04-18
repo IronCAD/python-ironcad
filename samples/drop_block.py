75a87abc2c65a24143d301b1e738325b0f484f40
Nathan Vrieland <NVrieland@ironcad.com>
2026-04-18 10:19:06 -0400

    added sample

---
+++ samples/drop_block.py
@@ -0,0 +1,29 @@
+import python_ironcad
+from python_ironcad import ICAPI
+
+IRONCAD = python_ironcad.IronCAD()
+IRONCAD.attach()
+IZBase : ICAPI.IZBaseApp = IRONCAD.GetIZBaseApp()
+print(IZBase)
+catalogmgr : ICAPI.IZCatalogMgr = IZBase.CatalogMgr
+
+catalogs = [catalogmgr.Catalog(i) for i in range(catalogmgr.Count)]
+
+starter_catalog : ICAPI.IZCatalog = next((c for c in catalogs if c.Name == "Starter"), None)
+
+if starter_catalog is None:
+    print("Starter Catalog not found.")
+    exit(1)
+# starter_catalog.EntryCount
+entries = [starter_catalog.Entry(i) for i in range(starter_catalog.EntryCount)]
+extrude : ICAPI.IZCatalogEntry = next((e for e in entries if e.Name == "Extrude"), None)
+
+if extrude is None:
+    print("Extrude Entry not found.")
+    exit(1)
+
+extrude.DropObject(0,0)
+
+IZDocument : ICAPI.IZDoc = IZBase.ActiveDoc
+IZSceneDoc : ICAPI.IZSceneDoc = IZDocument.QueryInterface(ICAPI.IZSceneDoc)
+IZSceneDoc.Redraw()
